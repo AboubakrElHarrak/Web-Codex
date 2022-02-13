f6cea9b2f81f23da165238ffc9f45b7337142d52
Assifar-Karim <82780260+Assifar-Karim@users.noreply.github.com>
2022-02-13 01:35:32 +0100

    Add Oauth2 authorization server
    I implemented the Oauth2 authorization server that the app will use to
    authorize authenticated users to perform actions that require to be
    logged in to do.

---
+++ Backend/Oauth-authorization-server/src/main/java/com/codex/oauthserver/OauthAuthorizationServerApplication.java
@@ -0,0 +1,15 @@
+package com.codex.oauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthAuthorizationServerApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(OauthAuthorizationServerApplication.class, args);
+	}
+
+}
