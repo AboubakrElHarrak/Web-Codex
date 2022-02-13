076aa3299003405a23636429f51aafcd61356c34
Assifar-Karim <82780260+Assifar-Karim@users.noreply.github.com>
2022-02-13 03:25:55 +0100

    Add article search
    I added the article search functionnality using the elasticsearch
    search api paired with a multi-match query that returns the first
    15 best results based on the score calculated by elasticsearch.

--- Backend/spring-security-client/src/main/java/com/codex/machina/ex/homini/service/ArticleService.java
+++ Backend/spring-security-client/src/main/java/com/codex/machina/ex/homini/service/ArticleService.java
@@ -3,9 +3,11 @@ package com.codex.machina.ex.homini.service;
 import com.codex.machina.ex.homini.error.ArticleNotFoundException;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 public interface ArticleService
 {
     Map<String, Object> fetchArticleByTitle(String title) throws ArticleNotFoundException, IOException;
+    List<Map<String, Object>> findArticleBySearch(String searchQuery) throws ArticleNotFoundException, IOException;
 }
