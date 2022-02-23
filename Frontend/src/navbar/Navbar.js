4da718988cbc99c0bb960b0905169d9c27b27670
Assifar-Karim <82780260+Assifar-Karim@users.noreply.github.com>
2022-02-23 23:55:48 +0100

    Add time spent reading article counter
    I added the counter that handles counting the time spent reading
    a codex article. The time spent reading is then stored in the MySQL
    database so that we can use it as an article rating metric for
    the logged in users.

--- Frontend/src/navbar/Navbar.js
+++ Frontend/src/navbar/Navbar.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import {useNavigate,Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 export default function Navbar(props) {
-  let navigate=useNavigate();
   return <nav className="navbar navbar-expand-md  navbar-dark bg-dark text-white ">
             <div className="container">
-                <a className="navbar-brand fs-1 fw-bold" href='' onClick={()=>{navigate('/');}}>CODEX</a>
+                <a className="navbar-brand fs-1 fw-bold" href='' onClick={props.mainRate}>CODEX</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
                     <span className="navbar-toggler-icon"></span>
                 </button>
